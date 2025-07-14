85f23067e7786bb6cde20e7f53158f4d015838af
eshihanda <eshihandamonn@gmail.com>
2025-07-14 05:55:55 +0300

    worked on the projects sections

--- my-react-app/src/Projects.jsx
+++ my-react-app/src/Projects.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import './Styles/Projects.css';
+import keja from './assets/kejaprime.jpeg';
+import shop from './assets/shop.jpeg';
 
 const Projects = () => {
   return (
-    <section className="project-section position-relative">
-      {/* Top wave */}
-      <div className="custom-top-shape-curve"></div>
+    <>
+    <div className='top-curve-section'>
 
-      <div className="container py-5 text-center">
-        <h2 className="mb-4">My Work</h2>
-        <p className="lead text-muted">
-          Here are a few projects I have built so far
-        </p>
+    </div>
+    <section className='projects-section text-center'>
+      <div className='container py-5'>
+        <h2 className='mb-4'>My Current Live Projects</h2>
+        <p className='lead text-muted'>Here are a few projects I have created using my knowledge in React.js</p>
       </div>
-
-      {/* Bottom wave */}
-      <div className="custom-bottom-shape-curve"></div>
+      <div className='row'>
+        <div className='col-md-4 mb-4'>
+          <div className='project-card'>
+            <img src={keja} alt="kejaprime website" className='project-img' />
+            <div className='project-overlay'>
+              <div className='content'>
+                <h5>KejaPrime</h5>
+                <p>A real estate website that allows users to rent houses online. Built using python and fast api as well as react.js</p>
+                <a href="https://kejaprime.vercel.app" className='btn btn-light btn-sm' target="_blank" rel="noopener noreferrer"> View project</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='col-md-4 mb-4'>
+          <div className='project-card'>
+            <img src={shop} alt="shop-mtaani website" className='project-img' />
+            <div className='project-overlay'>
+              <div className='content'>
+                <h5>KejaPrime</h5>
+                <p>I built this project using React, Java Script, boostrap, and CSS. Basically, a buyer can locate seller online 
+                  in their local communities and buy from sellers. The project enables easy access to local products, promoting local e-commerce and convinience.
+                </p>
+                <a href="https://shop-mtaani-742u.vercel.app/" className='btn btn-light btn-sm' target="_blank" rel="noopener noreferrer"> View project</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
+    <div className='bottom-curve-section'></div>
+    </>
   );
 };
 
