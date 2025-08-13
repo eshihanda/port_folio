4ec0cbc16caedd88f696b13c5f1044d13878cedc
eshihanda <eshihandamonn@gmail.com>
2025-08-13 09:33:47 +0300

    minimalist refining

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
       <a href="#" className='brand'>Portfolio</a>
       <ul>
         <li className='active'><a href="#">Home </a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="projects">Projects</a></li>
-        <li><a href="techstack">Stack</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#techstack">Stack</a></li>
       </ul>
 
     </nav>
